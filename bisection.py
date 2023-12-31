c3db1c6b0131b5308b5534171b579c6e35830324
Enrico <enrico.melis.casa@gmail.com>
2023-12-31 18:16:08 +0100

    Update bisection.py
    - function is taken as input, along with the range (a, b)
    - calc_func(); calc_sign(), check_existence() are created
    - bisection_method() will be the recursive function that maneges everything

--- bisection.py
+++ bisection.py
@@ -1,7 +1,74 @@
-# The script still needs to be written
+# The script still needs to be finished
+
+import sympy as sp
+
+def calc_func(func_input, x, x0):
+    try:
+        func = sp.simplify(func_input)
+        f = sp.lambdify(x, func, 'math')
+        result = f(x0)
+        print(f'f(x) = {func} at x = {x0} is {result}')
+        return result
+
+    except Exception:
+        print(f"Error: {Exception}")
+
+def calc_sign(f_x):
+    if f_x < 0:
+        return 'negative'
+    else:
+        return 'positive'
+
+def check_existence(f_a, f_b):
+    if(f_a * f_b < 0):
+        existence = True
+    else:
+        existence = False
+    
+    return existence
+
+def bisection_method():
+    print("")
 
 def main():
-    print('This is going to be the main function for the program')
+    function_input = input("Enter a f(x): ")
+    x = sp.Symbol('x')
+
+    # Input Control
+    request_loop = True
+    while(request_loop):
+        a = float(input("Enter bottom value 'a': "))
+        b = float(input("Enter bottom value 'b': "))
+
+        if(a < b):
+            request_loop = False
+        
+
+    f_a = calc_func(function_input, x, a)
+    f_a_sign = calc_sign(f_a)
+    f_b = calc_func(function_input, x, b)
+    f_b_sign = calc_sign(f_b)
+
+    zero_exists = check_existence(f_a, f_b)
+
+    if(not(zero_exists)):
+        print("There is not solution in the given range, try with another function or another range.")
+    else:
+        M = float((a+b)/2)
+        f_M = calc_func(function_input, x, M)
+
+        if(f_M == 0):
+            print(f"Solution found: f(x) at x = {M} is a solution.")
+            return
+
+        f_M_sign = calc_sign(f_M)
+
+        if f_M_sign != f_a_sign:
+            zero_exists = check_existence(f_a, f_M)
+            print(zero_exists)
+        else:
+            zero_exists = check_existence(f_b, f_M)
+            print(zero_exists)
 
 
 main()
